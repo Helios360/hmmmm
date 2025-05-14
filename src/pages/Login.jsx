058918697f93eb2945ed2a27946f5c8d0ebb17bc
Helios360 <elias.treguier@gmail.com>
2025-05-14 15:48:44 +0200

    c'est bon mieux plus

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import { Form, Button, Container, Card, Row, Col } from "react-bootstrap";
+import { useNavigate } from "react-router";
 
-const LoginPage = () => {
+const LoginPage = () => {	
   const [formData, setFormData] = useState({
     email: "",
     password: "",
   });
+	const [error, setError] = useState(null);
+	const navigate = useNavigate();
 
   const handleChange = (e) => {
     setFormData({
@@ -14,16 +17,42 @@ const LoginPage = () => {
     });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // Handle login logic here
+    try {
+			const response = await fetch("https://offers-api.digistos.com/api/auth/login", {
+			method: "POST",
+			headers:{ "Content-Type":"application/json", "Accept": "application/json",},
+			body:JSON.stringify(formData),
+		});
+		if (!response.ok){
+			throw {
+				status: response.status,
+				message: data.message
+			};
+		}
+
+		const data = await response.json();
+		localStorage.setItem("token", data.token);
+		localStorage.setItem("tokenExpiration",data.expiration);
+		
+		navigate("/offres/professionnelles");
+		} catch (err) {
+			console.error(err);
+			if (err.status ===  401){
+				setError("Identifiants invalides. boowamp");
+			} else {
+				setError("Erreur de connexion jsp ptdr force");
+			}
+		}
+	};
+
+	
     // Don't forget to handle errors, both for yourself (dev) and for the client (via a Bootstrap Alert):
     //   - Show an error if credentials are invalid
     //   - Show a generic error for all other cases
     // On success, redirect to the Pro Offers page
     console.log("Login submitted:", formData);
-  };
-
   return (
     <Container className="d-flex justify-content-center align-items-center min-vh-100">
       <Row className="w-100 justify-content-center">
