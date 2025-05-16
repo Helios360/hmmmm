59d8054c5f9d0ead38522341f080b44fd474d2f6
Helios360 <elias.treguier@gmail.com>
2025-05-16 10:38:44 +0200

    c'est bon maintenant lol

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -27,6 +27,7 @@ const Login = () => {
           "Accept": "application/json"
         },
         body: JSON.stringify(formData),
+        credentials: "include",
       });
       
       const data = await response.json();
